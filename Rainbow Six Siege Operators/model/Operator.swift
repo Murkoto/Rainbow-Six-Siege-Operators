41c1a65b48c0a26f91af4c49117cbe791ec8091b
Cesa Satria Anwar <Cesa263@gmail.com>
2020-06-23 18:25:18 +0700

    additional

---
+++ Rainbow Six Siege Operators/model/Operator.swift
@@ -0,0 +1,18 @@
+//
+//  Operator.swift
+//  Rainbow Six Siege Operators
+//
+//  Created by Cesa Anwar on 18/06/20.
+//  Copyright © 2020 Cesa Anwar. All rights reserved.
+//
+
+import UIKit
+
+struct Operator {
+    let codename: String
+    let role: String
+    let icon: UIImage
+    let picture: UIImage
+    let side: Int
+    let bio: String
+}
